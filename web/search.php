f0b50ebe2d5daa0da353f57a8da800efa250d994
Patrik Andó <screadern@gmail.com>
2025-01-15 01:21:31 +0100

    Fix footer

--- web/search.php
+++ web/search.php
@@ -71,13 +71,9 @@ $renderArr = array(
 
 $responseContent = $App->render('search.html', $renderArr);
 
-// RENDER WITH FOOTER
-// $getFooterContent = fn(): string
-//     => $App->render('todolist-panel.html', array())
-//     . $App->render('footer.html', array());
-
 $getFooterContent = fn(): string
-    => $App->render('todolist-panel.html', array());
+    => $App->render('todolist-panel.html', array())
+    . $App->render('footer.html', array());
 
 /**
  * Here the search begins
